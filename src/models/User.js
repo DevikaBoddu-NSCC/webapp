df41b6db5055a8a4580012aa68427c06ae3cca45
DevikaBoddu13 <boddu.d@northeastern.edu>
2024-02-05 16:49:12 -0500

    Created database, sequelize, post, put methods

---
+++ src/models/User.js
@@ -0,0 +1,58 @@
+// models/User.js
+const Sequelize  = require('sequelize');
+const bcrypt = require('bcrypt');
+const model = (sequelize) => {
+    const User = sequelize.define('User', {
+        id : {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+            
+        },
+        first_name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        last_name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+            primaryKey: true,
+        },
+        account_created : Sequelize.DATE,
+        account_updated : Sequelize.DATE
+        
+    },
+    {
+        createdAt : 'account_created',
+        updatedAt : 'account_updated'
+    });
+
+    // User.beforeCreate(async (user, options) => {
+    //     // Hash the password before creating the user
+    //     const hashedPassword = await bcrypt.hash(user.password, 10);
+    //     user.password = hashedPassword;
+    // });
+    
+    // User.beforeUpdate(async (user, options) => {
+    // // Hash the password before updating the user
+    // if (user.changed('password')) {
+    //     const hashedPassword = await bcrypt.hash(user.password, 10);
+    //     user.password = hashedPassword;
+    // }
+    // });
+    
+    User.sync({ alter: true });
+    return User;
+    
+}
+
+module.exports = model;
