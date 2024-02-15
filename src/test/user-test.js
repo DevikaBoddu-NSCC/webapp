d98bb3d9cddd443a6eeb44d66c5a8d5cf0885ce9
Devika Boddu <113263166+DevikaBoddu13@users.noreply.github.com>
2024-02-15 15:46:08 -0500

    Update user-test.js

--- src/test/user-test.js
+++ src/test/user-test.js
@@ -3,117 +3,78 @@ const request = require('supertest');
 const app = require('../../server'); 
 const { sequelize } = require('../database/database');
 const userModel = require('../models/User');
+
 describe('/v1/user endpoint', () => {
     let allTestsPassed = true;
+
     describe('POST /v1/user', () => {
-        try{
-            it('POST & GET', async () => {
-                const userData = {
-                    first_name: 'a',
-                    last_name: 'b',
-                    username: 'dev3@gmail.com',
-                    password: 'password1'
-                };
-                console.log("post method")
-                setTimeout(async () => {
-                    const response = await request(app)
-                    .post('/v1/user')
-                    .send(userData)
-                    .expect(201);
-                    
-                    expect(response.body).to.have.property('id');
-                    expect(response.body.first_name).to.equal(userData.first_name);
-                    
-                }, 1000); 
-                const authHeader = Buffer.from('dev3@gmail.com:password1').toString('base64');
-                console.log("get", authHeader);
-                // setTimeout(async () => {
-                const response = await request(app)
-                    .get('/v1/user/self')
-                    .set('Authorization', `Basic ${authHeader}`)
-                    .expect(200); 
+        it('POST & GET', async () => {
+            const userData = {
+                first_name: 'a',
+                last_name: 'b',
+                username: 'dev3@gmail.com',
+                password: 'password1'
+            };
+            
+            console.log("post method");
+            
+            const response = await request(app)
+                .post('/v1/user')
+                .send(userData)
+                .expect(201)
+                .then(async () => {
+                    // Code to execute after successful POST request
+                    console.log('POST request successful');
+                    // Additional actions here
+                    const authHeader = Buffer.from('dev3@gmail.com:password1').toString('base64');
+                    console.log("get", authHeader);
+                    const response1 = await request(app)
+                        .get('/v1/user/self')
+                        .set('Authorization', `Basic ${authHeader}`)
+                        .expect(200); 
 
-                console.log(response.body.userResponse);
-                assert(response.body.hasOwnProperty('userResponse'), 'Response body should contain userResponse property');
-                assert(response.body.userResponse.hasOwnProperty('id'), 'userResponse should have id property');
-                assert.strictEqual(response.body.userResponse.first_name, 'a', 'First name should match');
-                assert.strictEqual(response.body.userResponse.last_name, 'b', 'Last name should match');
-                assert.strictEqual(response.body.userResponse.username, 'dev3@gmail.com', 'Username should match');
-           // }, 1000); 
-            });
-        }catch (error) {
-            console.error(error);
-            allTestsPassed = false;
-        }
+                    assert(response1.body.hasOwnProperty('userResponse'), 'Response body should contain userResponse property');
+                    assert(response1.body.userResponse.hasOwnProperty('id'), 'userResponse should have id property');
+                    assert.strictEqual(response1.body.userResponse.first_name, 'a', 'First name should match');
+                    assert.strictEqual(response1.body.userResponse.last_name, 'b', 'Last name should match');
+                    assert.strictEqual(response1.body.userResponse.username, 'dev3@gmail.com', 'Username should match');
+                });
 
-        
+        });
     });
 
-
-
-
-describe('PUT /v1/user/self', () => {
-
-    it('PUT & GET', async () => {
-        try{
+    describe('PUT /v1/user/self', () => {
+        it('should update user information', async () => {
             const authHeader = Buffer.from('dev3@gmail.com:password1').toString('base64');
-                const userDataput = {
-                    first_name: 'aa',
-                    last_name: 'bb',
-                    password: 'password12'
-                };
+            const userDataput = {
+                first_name: 'aa',
+                last_name: 'bb',
+                password: 'password12'
+            };
 
-                setTimeout(async () => {
-                    const response = await request(app)
-                    .put('/v1/user/self')
-                    .set('Authorization', `Basic ${authHeader}`)
-                    .send(userDataput)
-                    .expect(204);
-                
-                    expect(response.body).to.have.property('id');
-                    expect(response.body.first_name).to.equal(userDataput.first_name);
-                
-                
-                console.log(response.body.userResponse);
-                assert(response.body.hasOwnProperty('userResponse'), 'Response body should contain userResponse property');
-                assert(response.body.userResponse.hasOwnProperty('id'), 'userResponse should have id property');
-                assert.strictEqual(response.body.userResponse.first_name, 'aa', 'First name should match');
-                assert.strictEqual(response.body.userResponse.last_name, 'bb', 'Last name should match');
-                },1000);
-
-                const authHeaderGet = Buffer.from('dev3@gmail.com:password12').toString('base64');
-                //setTimeout(async () => {
-                    const response = await request(app)
+            const response = await request(app)
+                .put('/v1/user/self')
+                .set('Authorization', `Basic ${authHeader}`)
+                .send(userDataput)
+                .expect(204)
+                .then(async () => {
+                    // Code to execute after successful PUT request
+                    console.log('PUT request successful');
+                    // Additional actions here
+                    const authHeaderGet = Buffer.from('dev3@gmail.com:password12').toString('base64');
+                    const response1 = await request(app)
                         .get('/v1/user/self')
                         .set('Authorization', `Basic ${authHeaderGet}`)
                         .expect(200); 
-    
-                    console.log(response.body.userResponse);
-                    assert(response.body.hasOwnProperty('userResponse'), 'Response body should contain userResponse property');
-                    assert(response.body.userResponse.hasOwnProperty('id'), 'userResponse should have id property');
-                    assert.strictEqual(response.body.userResponse.first_name, 'aa', 'First name should match');
-                    assert.strictEqual(response.body.userResponse.last_name, 'bb', 'Last name should match');
-                    assert.strictEqual(response.body.userResponse.username, 'dev3@gmail.com', 'Username should match');
-                 //  },1000);
-        }catch (error) {
-            console.error(error);
-            allTestsPassed = false;
-        }
-    
+
+                    assert(response1.body.hasOwnProperty('userResponse'), 'Response body should contain userResponse property');
+                    assert(response1.body.userResponse.hasOwnProperty('id'), 'userResponse should have id property');
+                    assert.strictEqual(response1.body.userResponse.first_name, 'aa', 'First name should match');
+                    assert.strictEqual(response1.body.userResponse.last_name, 'bb', 'Last name should match');
+                    assert.strictEqual(response1.body.userResponse.username, 'dev3@gmail.com', 'Username should match');
+                });
+
+        });
     });
-});
-
-
-
-
-after(() => {
-    if (allTestsPassed) {
-        process.exit(0); // All tests passed, exit with code 0
-    } else {
-        process.exit(1); // At least one test failed, exit with code 1
-    }
-});
 
 });
-
-
